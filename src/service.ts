d34d05e15128833805ac3ebf8cccdfb1e564a659
vishaalsehgal <sehgalvishal816@gmail.com>
2023-03-28 14:12:17 +0530

    changed api url

--- src/service.ts
+++ src/service.ts
@@ -1,25 +1,27 @@
 import axios from "axios";
 
+const url = `https://book-reader-backend.vercel.app/api/v1`
+
 export const getAllBooks = async () => {
-  const result = await axios.get(`http://localhost:8000/api/v1/books`);
+  const result = await axios.get(`${url}/books`);
   return result.data;
 };
 
 export const getBookById = async (id: number) => {
-  const result = await axios.get(`http://localhost:8000/api/v1/book/${id}`);
+  const result = await axios.get(`${url}/book/${id}`);
   return result.data;
 };
 export const ratingBook = async (id: number, rating: number) => {
   let payload: any = { rating };
   const result = await axios.post(
-    `http://localhost:8000/api/v1/rateBook/${id}`,
+    `${url}/rateBook/${id}`,
     payload
   );
   return result.data;
 };
 export const addBook = async (payload: any) => {
   const result = await axios.post(
-    `http://localhost:8000/api/v1/addBook`,
+    `${url}/addBook`,
     payload,
     {
       headers: {
